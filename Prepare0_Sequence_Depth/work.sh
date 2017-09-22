3fd85167ce664947df45c6d3d0a3dba61e113c7c
JinfengChen <jinfeng7chen@gmail.com>
2017-09-21 17:06:32 -0700

    Sequence depth

--- Prepare0_Sequence_Depth/work.sh
+++ Prepare0_Sequence_Depth/work.sh
@@ -2,6 +2,7 @@ echo "depth"
 sed 's/GN//' RILs_ALL_bam_correct_merged.summary | sort -k1n > RILs_ALL_bam_correct_merged.sorted.summary
 
 echo "coverage"
+cp ~/BigData/00.RD/RILs/QTL_pipe/bin/RILs_ALL_272line/NB.RILs.dbSNP.SNPs.RILs ./
 python NeedCare.py > RILs_ALL_272.needcare.txt &
 
 echo "Generate Depth.table"
